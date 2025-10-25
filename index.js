f38a6478f9bcc73364b27422d701e7bdfe395a9f
Tanish <tanishsingh@866gamil.com>
2025-10-25 14:50:10 +0530

    Added game logic

---
+++ index.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", () => {
+    let outer = document.getElementById("outer");
+    let chance = false;
+    let arr = Array(9).fill(undefined);
+    outer.addEventListener("click", (e) => {
+        let cell = e.target;
+        let cellNumber = cell.getAttribute("data-cell");
+        console.log(cellNumber);
+
+        if(cell.getAttribute("data-clicked")){
+            return;
+        }
+        cell.setAttribute("data-clicked", "true");
+        if(chance == true){
+            cell.textContent = "X";
+            arr[cellNumber] = "X";
+            winningCombo("X");
+        }
+        else{
+            cell.textContent = "O";
+            arr[cellNumber] = "O"
+            winningCombo("O");
+        }
+        console.log(arr);
+        chance = !chance;  // toggle chance
+    });
+
+    function winningCombo(char){
+        if(arr[0]==char && arr[1]==char && arr[2]==char){
+            // the f0th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+        if(arr[3]==char && arr[4]==char && arr[5]==char){
+            // the f1th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+        if(arr[6]==char && arr[7]==char && arr[8]==char){
+            // the f2th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+        if(arr[0]==char && arr[3]==char && arr[6]==char){
+            // the f3th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+        if(arr[1]==char && arr[4]==char && arr[7]==char){
+            // the f0th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+        if(arr[2]==char && arr[5]==char && arr[8]==char){
+            // the f0th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+        if(arr[0]==char && arr[4]==char && arr[8]==char){
+            // the f0th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+        if(arr[2]==char && arr[4]==char && arr[6]==char){
+            // the f0th row having the char
+            alert(`${char} wins!`);
+            window.location.reload();
+        }
+    }
+});  
